54ec95185c7c934cba5416cb5639b35121ac376e
Lucas Han <ldhan0715@gmail.com>
2020-06-22 11:07:46 +0900

    Update Source Codes

--- search.js
+++ search.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector("#searchForm");
 const searchInput = searchForm.querySelector("input")
+const searchIcon = searchForm.querySelector("img");
 
 function handleSearch(event) {
     event.preventDefault();
@@ -8,8 +9,23 @@ function handleSearch(event) {
     location.replace(`https://www.google.com/search?q=${searchQuery}`)
 }
 
+/* Beta Features
+function searchInactive() {
+    searchIcon.src = "search.png";
+}
+
+function searchActive() {
+    searchIcon.src = "search_active.png";
+}
+*/
+
 function init() {
     searchForm.addEventListener("submit",handleSearch);
+    /* Beta Features
+    searchInput.addEventListener("mouseover", searchActive);
+    searchInput.addEventListener("mouseout", searchInactive);
+    searchInput.addEventListener("focus", searchActive);
+    */
 }
 
 init();
