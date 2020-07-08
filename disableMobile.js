f4d6f55f13ed9ec9ba8e1f0c40d6f1e7e4037d34
Lucas Han <ldhan0715@gmail.com>
2020-07-08 11:41:21 +0900

    Update

--- disableMobile.js
+++ disableMobile.js
@@ -1,21 +1,6 @@
-const html = document.querySelector("html");
-
-function clearScreen() {
-    const body = document.querySelector("body");
-    const nbody = document.createElement("nbody");
-    const text = document.createElement("div");
-    text.innerText = "Not For Mobile.";
-    text.style.fontSize = "2.5rem";
-    text.style.fontFamily = "Times New Roman";
-    html.removeChild(body);
-    nbody.appendChild(text);
-    html.appendChild(nbody);
-}
-
 function checkMobile() {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        alert("[Alert] Artrium ToDo is not for Mobile, but PC. Thank you.");
-        clearScreen();
+        location.replace("blocked.html");
     }
 }
 
